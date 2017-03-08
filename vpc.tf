244dd42a1f83ca7447ea1999ea7ffa817e7f115f
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-03-09 02:10:34 +0500

    Add security group

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "default" {
+ resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
     enable_dns_hostnames = true
@@ -37,4 +37,25 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public" {
     subnet_id = "${aws_subnet.public.id}"
     route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_security_group" "fastai" {
+    name = "fast-ai-security-group"
+    description = "SG for fast.ai machine"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 8888
+        to_port = 8898
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    vpc_id = "${aws_vpc.default.id}"
 }
